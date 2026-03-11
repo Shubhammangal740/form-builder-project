fbb29d4754aa815605a3e3387e5f9956d615b13f
Shubhammangal740 <shubhammangal740@gmail.com>
2026-03-12 00:13:11 +0530

    change the backend url

--- Frontend/src/utils/constants.js
+++ Frontend/src/utils/constants.js
@@ -1,5 +1,6 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://form-builder-backend-wnlq.onrender.com/api";
 
 export const FIELD_TYPES = {
   TEXT: "text",
